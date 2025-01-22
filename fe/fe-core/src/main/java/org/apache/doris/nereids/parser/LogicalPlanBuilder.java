a783a59232f8054169d663bbc2925fdcf9b9274c
LiBinfeng <libinfeng@selectdb.com>
2025-01-22 10:50:59 +0800

    fix compile problem

--- fe/fe-core/src/main/java/org/apache/doris/nereids/parser/LogicalPlanBuilder.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/parser/LogicalPlanBuilder.java
@@ -17,7 +17,6 @@
 
 package org.apache.doris.nereids.parser;
 
-import jdk.internal.org.jline.reader.Parser;
 import org.apache.doris.alter.QuotaType;
 import org.apache.doris.analysis.ArithmeticExpr.Operator;
 import org.apache.doris.analysis.BrokerDesc;
@@ -865,7 +864,7 @@ public class LogicalPlanBuilder extends DorisParserBaseVisitor<Object> {
         return new CreateJobCommand(createJobInfo);
     }
 
-    private void checkJobNameKey(String key, String keyFormat, Parser.ParseContext parseContext) {
+    private void checkJobNameKey(String key, String keyFormat, DorisParser.SupportedJobStatementContext parseContext) {
         if (key.isEmpty() || !key.equalsIgnoreCase(keyFormat)) {
             throw new ParseException(keyFormat + " should be: '" + keyFormat + "'", parseContext);
         }
